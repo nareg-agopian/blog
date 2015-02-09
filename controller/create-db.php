4300ef4c82b446ca3015ca52613f11a5bae0845a
nareg-agopian <nareg19@outlook.com>
2015-02-09 10:45:31 -0800

    no message

--- controller/create-db.php
+++ controller/create-db.php
@@ -5,7 +5,7 @@ require_once(__DIR__ . "/../model/database.php");
 $connection = new mysqli($host, $username, $password);
 
 if ($connection->connect_error) {
-    die("Error: " . $connection->connect_error);
+    die("<p>Error: " . $connection->connect_error . "</p>");
 } else {
     echo "Success" . $connection->host_info;
 }
@@ -16,11 +16,27 @@ if(!$exists) {
     $query = $connection->query("CREATE DATABASE $database");
     
     if($query) {
-        echo "successfully created data base: " . $database;
+        echo "<p>Successfully created data base: " . $database . "</p>";
     }
-    
-
+    else {
+        echo "<p>Database already exists.</p>";
     }
 
-
+      }
+      
+      $query = $connection->query("CREATE TABLE posts ("
+             . "id int(11) NOT NULL AUTO_INCERMENT,"
+             . "title varchar(255) NOT NULL"
+             . "post text NOT NULL"
+              
+             . "PRIMARY  KEY (id))");
+      
+      
+      if($query) {
+          echo "<p>Succesfully created table: post<p/>";
+      }
+      else {
+          echo "<p>$connection->error</p>";
+      }
+      
 $connection->close();
